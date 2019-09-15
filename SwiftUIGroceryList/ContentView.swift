76eed8e16531d54a703704a0c97d1dee587e6aff
Paul Carroll <p.v.carroll@gmail.com>
2019-09-15 13:05:45 -0500

    Initial Commit

---
+++ SwiftUIGroceryList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGroceryList
+//
+//  Created by Paul Carroll on 9/15/19.
+//  Copyright © 2019 Paul Carroll. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
